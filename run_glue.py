1904f738afda14d7c48c6bf32aee9849ed069351
Harbin Hong <hhong61502@gmail.com>
2023-04-28 18:29:10 -0400

    should fix not starting bug

--- run_glue.py
+++ run_glue.py
@@ -556,9 +556,9 @@ def main():
             return tokenizer(examples[text_column_name], return_special_tokens_mask=True)
         
         # change back once done testing
-        myTest = Dataset.from_dict(datasets["test"][1:100])
-        myTrain = Dataset.from_dict(datasets["train"][1:100])
-        myVal = Dataset.from_dict(datasets["validation"][1:100])
+        myTest = Dataset.from_dict(datasets["test"][1:100000])
+        myTrain = Dataset.from_dict(datasets["train"][1:100000])
+        myVal = Dataset.from_dict(datasets["validation"][1:100000])
         myDatasets = DatasetDict({"test":myTest, "train":myTrain, "validation": myVal})
 
         tokenized_datasets = myDatasets.map(
