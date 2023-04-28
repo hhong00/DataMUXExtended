7f94a52aaa2827bd37a58f63c7e848a7b4c92560
Harbin Hong <hhong61502@gmail.com>
2023-04-28 14:52:47 -0400

    smaller dataset

--- run_glue.py
+++ run_glue.py
@@ -556,9 +556,9 @@ def main():
             return tokenizer(examples[text_column_name], return_special_tokens_mask=True)
         
         # change back once done testing
-        myTest = Dataset.from_dict(datasets["test"][1:10])
-        myTrain = Dataset.from_dict(datasets["train"][1:10])
-        myVal = Dataset.from_dict(datasets["validation"][1:10])
+        myTest = Dataset.from_dict(datasets["test"][1:100])
+        myTrain = Dataset.from_dict(datasets["train"][1:100])
+        myVal = Dataset.from_dict(datasets["validation"][1:100])
         myDatasets = DatasetDict({"test":myTest, "train":myTrain, "validation": myVal})
         tokenized_datasets = myDatasets.map(
             tokenize_function,
