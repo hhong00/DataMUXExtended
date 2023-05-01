3faa2bfce11b51e9ea5e6dc00576b0aaac5ce260
Harbin Hong <hhong61502@gmail.com>
2023-05-01 11:53:53 -0400

    testing for overfit

--- run_glue.py
+++ run_glue.py
@@ -555,9 +555,9 @@ def main():
             return tokenizer(examples[text_column_name], return_special_tokens_mask=True)
         
         # change back once done testing
-        myTest = Dataset.from_dict(datasets["test"][1:1000])
-        myTrain = Dataset.from_dict(datasets["train"][1:1000])
-        myVal = Dataset.from_dict(datasets["validation"][1:1000])
+        myTest = Dataset.from_dict(datasets["test"][1:5000])
+        myTrain = Dataset.from_dict(datasets["train"][1:5000])
+        myVal = Dataset.from_dict(datasets["validation"][1:5000])
         myDatasets = DatasetDict({"test":myTest, "train":myTrain, "validation": myVal})
         #for normal run thru whole dataset
         #myDatasets = datasets
