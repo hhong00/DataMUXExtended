38118ac7a44d43f6632d918a7891ebecfaf5c461
Harbin Hong <hhong61502@gmail.com>
2023-05-02 01:33:33 -0400

    testing if actually using lstm

--- run_glue.py
+++ run_glue.py
@@ -451,6 +451,7 @@ def main():
             #if model_path_supplied:
                 #model = LSTMSequenceClassificationMuxed.from_pretrained(model_args.model_name_or_path, config=config)
             #else:
+            breakpoint()
             model = LSTMSequenceClassificationMuxed(config=config)
         else:
             if model_path_supplied:
