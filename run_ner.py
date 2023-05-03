9986964307dfa8d40be7bb3f28900b553a50de29
Harbin Hong <hhong61502@gmail.com>
2023-05-03 12:02:54 -0400

    testing

--- run_ner.py
+++ run_ner.py
@@ -437,7 +437,7 @@ def main():
             if model_path_supplied:
                 model = AutoModelForTokenClassification.from_pretrained(model_args.model_name_or_path, config=config)
             else:
-                breakpoint()
+                config = AutoConfig.from_pretrained("albert-base-v2")
                 model = AutoModelForTokenClassification(config=config)
 
     # Tokenizer check: this script requires a fast tokenizer.
