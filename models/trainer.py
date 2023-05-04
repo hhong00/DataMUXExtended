a1d49bef5b513558bfcdfb9e5c5f92dc6cf0b175
Harbin Hong <hhong61502@gmail.com>
2023-05-04 14:31:32 -0400

    testing

--- models/trainer.py
+++ models/trainer.py
@@ -579,7 +579,7 @@ class MuxTrainer(Trainer):
             logger.info(f"Loading model from {resume_from_checkpoint}).")
             if lstm:
                 
-                self.model.load_state_dict(torch.load("datamux/lstm/pretraining/model.pt/model.pt"))
+                self.model.load_state_dict(torch.load("datamux/lstm/pretraining/model.pt"))
             else:
                 if isinstance(self.model, PreTrainedModel):
                     self.model = self.model.from_pretrained(resume_from_checkpoint)
